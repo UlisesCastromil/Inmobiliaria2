4c6456c4ea531193e877df96b484844b3ba419b4
HP <HP@DESKTOP-NLQU346>
2023-06-27 16:01:31 -0300

    Agregar archivos de proyecto.

---
+++ Inmobiliaria/Models/Clientes.cs
@@ -0,0 +1,8 @@
+﻿namespace Inmobiliaria.Models
+{
+    public class Clientes
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+    }
+}
---
+++ Inmobiliaria/Models/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inmobiliaria.Models
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Clientes> Clientes { get; set; } 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=InmbobiliariaDataBase;Trusted_Connection=True;TrustServerCertificate=true");
+        }
+    }
+}
